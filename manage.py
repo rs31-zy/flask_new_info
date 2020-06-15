f43033baef725954e5a700e658d350a04e298514
rs31-zy <1137111380@qq.com>
2020-06-15 19:39:18 +0800

    项目拆分

--- manage.py
+++ manage.py
@@ -4,47 +4,15 @@
 # @Email   : li.yan_li@neusoft.com
 # @File    : manage.py.py
 # @Software: PyCharm
-import redis
-from flask import Flask, session
-from flask_session import Session
-from flask_sqlalchemy import SQLAlchemy
-from flask_wtf import CSRFProtect
+from flask import session
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
+from info import app,db
 
-app = Flask(__name__)
-
-class Config(object):
-    """工程信息配置"""
-    SECRET_KEY = "EjpNVSNQTyGi1VvWECj9TvC/+kq3oujee2kTfQUs8yCM6xX9Yjq52v54g+HVoknA"
-
-    DEBUG = True
-    # 导入数据库配置
-    # 设置数据库连接
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:12345678@127.0.0.1:3306/information1'
-    # 动态追踪设置
-    app.config['SQLALCHEMY_TRACK_MODUFICATIONS'] = True
-    # 显示原始sql
-    app.config['SQLALCHEMY_ECHO'] = True
-
-    REDIS_HOST = "127.0.0.1"
-    REDIS_PORT = 6379
-    # flask_session的配置信息
-    SESSION_TYPE = "redis"  # 指定 session 保存到 redis 中
-    SESSION_USE_SIGNER = True  # 让 cookie 中的 session_id 被加密签名处理
-    SESSION_REDIS = redis.StrictRedis(host=REDIS_HOST, port=REDIS_PORT)  # 使用 redis 的实例
-    SESSION_PERMANENT = False
-    PERMANENT_SESSION_LIFETIME = 86400  # session 的有效期，单位是秒
-
-
-app.config.from_object(Config)
-db = SQLAlchemy(app)
-# 初始化redis配置
-# redis.StrictRedis(host=Config.RDIES_HOST, port=Config.RDIES_PORT)
-
-# 开启csrf 保护， 只用于服务器验证功能
-CSRFProtect(app)
-# 设置session保存指定位置
-Session(app)
-
+manager = Manager(app)
+#数据库迁移
+Migrate(app, db)
+manager.add_command('db', MigrateCommand)
 
 
 @app.route('/')
@@ -52,4 +20,4 @@ def index():
     session['name'] = 'eric'
     return "index"
 if __name__ == '__main__':
-    app.run()
+    manager.run()
