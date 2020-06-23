1a2f744037815bbc28e6178c55ad03d940cff5e9
rs31-zy <1137111380@qq.com>
2020-06-23 17:49:51 +0800

    新闻列表加载1

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -4,10 +4,11 @@
 # @Email   : li.yan_li@neusoft.com
 # @File    : views.py
 # @Software: PyCharm
-from flask import render_template, current_app, session
+from flask import render_template, current_app, session, request, jsonify
 
 from info import constants
-from info.models import User, News
+from info.models import User, News, Category
+from info.utils.response_code import RET
 from . import index_blu
 @index_blu.route('/')
 def index():
@@ -28,11 +29,21 @@ def index():
 
     click_news_list = []
     for news in news_list if news_list else []:
-        click_news_list.append(news.to_dict())
+        click_news_list.append(news.to_basic_dict())
+
+
+    category_dicts = []
+    #获取新闻分类数据
+    categorys = Category.query.all()
+    for category in categorys:
+        category_dicts.append(category.to_dict())
+
+
 
     data = {
         'user_info': user.to_dict() if user else None,
         "click_news_list":click_news_list,
+        "categorys":category_dicts
     }
 
 
@@ -44,3 +55,50 @@ def index():
 def get_web_logo():
 
     return current_app.send_static_file('news/favicon.ico')
+
+
+@index_blu.route('/newslist')
+def get_news_list():
+    '''
+    1. 获取参数
+    2. 校验参数
+    3. 查询数据
+    4. 返回数据
+    :return:
+    '''
+
+    args_dict = request.args
+
+    page = args_dict.get('page',1)
+    per_page = args_dict.get('per_page',constants.HOME_PAGE_MAX_NEWS)
+
+    category_id = args_dict.get('cid', 1)
+
+    try:
+        page = int(page)
+        per_page = int(per_page)
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.PARAMERR,errmsg="参数错误")
+
+    filters = []
+    #查询数据并分页
+    if category_id != '1':
+        filters.append(News.category_id == category_id)
+
+    try:
+        paginate = News.query.filter(*filters).order_by(News.create_time.desc()).paginate(page,per_page)
+        #获取查询的数据
+        items = paginate.items
+        #获取总页数
+        total_page = paginate.pages
+        current_page = paginate.page
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.DBERR,errmsg="数据查询失败")
+
+    news_li = []
+    for news in items:
+        news_li.append(news.to_basic_dict())
+
+    return jsonify(errno=RET.OK,totalPage=total_page,currentPage=current_page,newsList=news_li,cid=category_id)
--- info/static/news/js/index.js
+++ info/static/news/js/index.js
@@ -5,24 +5,26 @@ var data_querying = true;   // 是否正在向后台获取数据
 
 
 $(function () {
+    //调用updateNewsData方法，更新数据
+    updateNewsData();
     // 首页分类切换
     $('.menu li').click(function () {
-        var clickCid = $(this).attr('data-cid')
+        var clickCid = $(this).attr('data-cid');
         $('.menu li').each(function () {
             $(this).removeClass('active')
-        })
-        $(this).addClass('active')
+        });
+        $(this).addClass('active');
 
         if (clickCid != currentCid) {
             // 记录当前分类id
-            currentCid = clickCid
+            currentCid = clickCid;
 
             // 重置分页参数
-            cur_page = 1
-            total_page = 1
+            cur_page = 1;
+            total_page = 1;
             updateNewsData()
         }
-    })
+    });
 
     //页面滚动加载相关
     $(window).scroll(function () {
@@ -47,4 +49,31 @@ $(function () {
 
 function updateNewsData() {
     // TODO 更新新闻数据
+    var params = {
+        "page":1,
+        "per_page":10,
+        "cid":currentCid
+    }
+
+    $.get("/newslist",params,function (resp) {
+        if(resp){
+            //清空原来数据
+            $(".list_con").html("")
+            //显示数据
+            for(var i = 0; i < resp.newsList.length; i++){
+                var news = resp.newsList[i]
+                var content = '<li>'
+                content += '<a href="#" class="news_pic fl"><img src="' + news.index_image_url + '?imageView2/1/w/170/h/170"></a>'
+                content += '<a href="#" class="news_title fl">' + news.title + '</a>'
+                content += '<a href="#" class="news_detail fl">' + news.digest + '</a>'
+                content += '<div class="author_info fl">'
+                content += '<div class="source fl">来源：' + news.source + '</div>'
+                content += '<div class="time fl">' + news.create_time + '</div>'
+                content += '</div>'
+                content += '</li>'
+                $(".list_con").append(content)
+            }
+
+        }
+    })
 }
